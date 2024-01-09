a2f5f00bc8f78d123fa247e4a06435cf3967d0e2
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2024-01-09 10:06:36 +0100

    Fix flying fish

---
+++ src/main/java/dev/doublekekse/boids/goals/StayInWaterGoal.java
@@ -0,0 +1,46 @@
+package dev.doublekekse.boids.goals;
+
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.phys.Vec3;
+
+public class StayInWaterGoal extends Goal {
+    private final Mob mob;
+
+    public StayInWaterGoal(Mob mob) {
+        this.mob = mob;
+    }
+
+    @Override
+    public boolean canUse() {
+        return true;
+    }
+
+    @Override
+    public void tick() {
+        var blockPos = mob.blockPosition();
+        var blockAbove = mob.level().getBlockState(blockPos.above(2));
+        var blockBelow = mob.level().getBlockState(blockPos.below(1));
+        var amount = amount();
+
+        if(blockAbove.getFluidState().isEmpty()) {
+            mob.addDeltaMovement(new Vec3(0, -amount, 0));
+        }
+
+        if(blockBelow.getFluidState().isEmpty()) {
+            mob.addDeltaMovement(new Vec3(0, amount, 0));
+        }
+    }
+
+    float amount() {
+        var amount = 0.1f;
+        var dY = Mth.abs((float) mob.getDeltaMovement().y);
+
+        if (dY > amount) {
+            amount = dY;
+        }
+
+        return amount;
+    }
+}
