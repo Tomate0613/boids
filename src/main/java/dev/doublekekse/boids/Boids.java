6e897b3741a26c58b4c6ed285f6f4ffd960bd81a
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2024-05-25 17:12:52 +0200

    Add basic config
    (cherry picked from commit df6111812c0148827febc83cd5cd1ab511e628c5)

---
+++ src/main/java/dev/doublekekse/boids/Boids.java
@@ -0,0 +1,27 @@
+package dev.doublekekse.boids;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import dev.doublekekse.boids.config.BoidsConfig;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+
+import static net.minecraft.commands.Commands.literal;
+
+public class Boids implements ModInitializer {
+    public static BoidsConfig CONFIG = BoidsConfig.load();
+
+    @Override
+    public void onInitialize() {
+        CommandRegistrationCallback.EVENT.register(
+            (dispatcher, registryAccess, environment) -> {
+                dispatcher.register(
+                    literal("boids").requires((source) -> source.hasPermission(2)).then(literal("config").then(literal("reload").executes(ctx -> {
+                        CONFIG = BoidsConfig.load();
+
+                        return 1;
+                    })))
+                );
+            }
+        );
+    }
+}
