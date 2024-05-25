df6111812c0148827febc83cd5cd1ab511e628c5
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2024-05-25 17:12:52 +0200

    Add basic config

--- src/main/java/dev/doublekekse/boids/mixin/AbstractSchoolingFishMixin.java
+++ src/main/java/dev/doublekekse/boids/mixin/AbstractSchoolingFishMixin.java
@@ -1,5 +1,6 @@
 package dev.doublekekse.boids.mixin;
 
+import dev.doublekekse.boids.Boids;
 import dev.doublekekse.boids.goals.BoidGoal;
 import dev.doublekekse.boids.goals.LimitSpeedAndLookInVelocityDirectionGoal;
 import dev.doublekekse.boids.goals.StayInWaterGoal;
@@ -12,6 +13,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static dev.doublekekse.boids.Boids.CONFIG;
+
 @Mixin(AbstractSchoolingFish.class)
 public abstract class AbstractSchoolingFishMixin extends AbstractFish {
     public AbstractSchoolingFishMixin(EntityType<? extends AbstractFish> entityType, Level level) {
@@ -20,9 +23,14 @@ public abstract class AbstractSchoolingFishMixin extends AbstractFish {
 
     @Inject(method = "registerGoals", at = @At("HEAD"), cancellable = true)
     protected void registerGoals(CallbackInfo ci) {
-        this.goalSelector.addGoal(5, new BoidGoal(this, 0.5f, 0.9f, 8 / 20f, 1 / 20f));
+        var type = this.getType().toString();
+
+        if(CONFIG.excludeEntities.contains(type))
+            return;
+
+        this.goalSelector.addGoal(5, new BoidGoal(this, CONFIG.separationInfluence, CONFIG.separationRange, CONFIG.alignmentInfluence, CONFIG.cohesionInfluence));
         this.goalSelector.addGoal(3, new StayInWaterGoal(this));
-        this.goalSelector.addGoal(2, new LimitSpeedAndLookInVelocityDirectionGoal(this, 0.3f, 0.8f));
+        this.goalSelector.addGoal(2, new LimitSpeedAndLookInVelocityDirectionGoal(this, CONFIG.minSpeed, CONFIG.maxSpeed));
 
         ci.cancel();
     }
