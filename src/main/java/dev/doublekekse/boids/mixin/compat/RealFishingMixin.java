50334d636025f1ed26cbd17012aafc7a8e4febde
Joseph Maria <josephmt1176@gmail.com>
2025-12-10 12:28:33 -0500

    Solves issue #27 - game no longer crashes when fishing real is installed

--- src/main/java/dev/doublekekse/boids/mixin/compat/RealFishingMixin.java
+++ src/main/java/dev/doublekekse/boids/mixin/compat/RealFishingMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FishingReal.class)
 public class RealFishingMixin {
-    @Inject(method = "convertItemStack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Mob;finalizeSpawn(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/DifficultyInstance;Lnet/minecraft/world/entity/MobSpawnType;Lnet/minecraft/world/entity/SpawnGroupData;Lnet/minecraft/nbt/CompoundTag;)Lnet/minecraft/world/entity/SpawnGroupData;"))
+    @Inject(method = "convertItemStack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Mob;finalizeSpawn(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/DifficultyInstance;Lnet/minecraft/world/entity/MobSpawnType;Lnet/minecraft/world/entity/SpawnGroupData;)Lnet/minecraft/world/entity/SpawnGroupData;"))
     private static void enable(ItemStack itemstack, Player player, Vec3 position, CallbackInfoReturnable<Entity> cir, @Local Mob mob) {
         ((MobDuck) mob).boids$disable();
     }
